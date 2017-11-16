66e7b6292dadd206fe7953bf6320abc9cf893e54
Austin Ray <austin.duke.ray@gmail.com>
2017-11-16 13:23:51 -0500

    Initial work on performance util for benchmark
    Started initial work on benchmarking utilities that will be used to
    monitor each algorithm's performance.

---
+++ genetic_algorithm/src/main/kotlin/io/ray/BenchmarkUtil.kt
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2017.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package io.ray
+
+import com.sun.management.OperatingSystemMXBean
+
+/**
+ * Calculate the memory usage of a Runtime.
+ * @param rt    Runtime of the object
+ * @return      Memory used by the runtime in bytes.
+ */
+fun getMemoryUsage(rt: Runtime) : Long = rt.totalMemory() - rt.freeMemory()
+
+/**
+ * Get the CPU usage of the process.
+ * @param osmxb   OperatingSystemMXBean that contains the process.
+ */
+fun getCpuUsage(osmxb: OperatingSystemMXBean) : Double = osmxb.processCpuLoad
+
+fun bytesToKiloBytes(bytes: Long) = bytes / 1024
+
+fun bytesToMegaBites(bytes: Long) = bytesToKiloBytes(bytes) / 1024
